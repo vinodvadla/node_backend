286ec2d06b422baca6b3b87da4988a03c03481d1
vadla vinod <vinodvinod0979@gmail.com>
2024-06-06 01:05:35 +0530

    updated

--- models/index.js
+++ models/index.js
@@ -12,7 +12,10 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    ...config,
+    dialectModule: mysql2,
+  });
 } else {
   sequelize = new Sequelize(
     config.database,
